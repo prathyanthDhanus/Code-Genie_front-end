0f6ffaf08bda02c9b0d3064bd712a02a542e95b6
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2023-09-11 12:55:27 +0530

    Started creating details of a student

--- src/Admin/singleStudentDetails.jsx
+++ src/Admin/singleStudentDetails.jsx
@@ -1,57 +1,169 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import { useRef } from 'react';
-import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { useState } from 'react';
-import CustomCard from '../Components/card';
-import Studentdetails from './studentdetails';
+
 
 const SingleStudentDetails = () => {
+  const inputRef = useRef();
+  const [defaultValue,setDefaultValue] = useState("")
 
-  const [student,setStudent] = useState(null);
-  const inputRef  = useRef();
-
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
-  const  id = inputRef.current.value;
-
-  try{
+const handleSearch = async ()=>{
+   const id = inputRef.current.value;
+ try{
     const response = await axios.get(`http://localhost:3000/admin/student/${id}`);
-    // console.log(response.data.data)
-    setStudent(response.data.data)
-   
-  }catch(error){
-    console.log(error);
-  }
-
-  }
-
-  useEffect(() => {
-    // This code will run after 'student' has been updated.
-    // console.log(student);
-  }, [student]);
-   
-  // useEffect(()=>{
-  //   const fetchData = async ()=>{
-
-  //     const response = await axios.get('http://localhost:3000//admin/student/')
-  //   }
-  // })
-    
+       const data = response.data;
+       setDefaultValue(data)
+  console.log(response);
+ }catch(error){
+  console.log("Error",error)
+ }
+}
+console.log(defaultValue);
 
   return (
+
     <div style={{width:"80%"}} >
-<h3>Single Student Details</h3>
-<Container fluid>
+<h3> Details of a Student </h3>
 
-              {student && (
-         <p>gg</p>
-        )}
+<Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+    <div className='d-flex' style={{alignItems:'center', justifyContent:'center'}}>
+    <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search for a student by ID"
+              className="me-2"
+              aria-label="Search"
+              ref={inputRef}
+            />
+          </Form>
+          <Button variant="outline-success" onClick={handleSearch}>Search</Button>
 
-</Container>
+    </div>
+      <div >
+    
+      <TextField
+          id="outlined-read-only-input"
+          label="Username"
+          defaultValue=''
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+     
+        <TextField
+          id="outlined-read-only-input"
+          label="Email"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          id="outlined-read-only-input"
+          label="Batch-Number"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        
+      </div>
+      
+       
+      <div>
+      <h5>Profile Details Section</h5>
+      <TextField
+          id="outlined-read-only-input"
+          label="Full-Name"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          id="outlined-read-only-input"
+          label="Phone-Number"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          id="outlined-read-only-input"
+          label="Pincode"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div>
+      <TextField
+          id="outlined-read-only-input"
+          label="Blood_Group"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      <TextField
+          id="outlined-multiline-static"
+          label="Full_Address"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+          <TextField
+          id="outlined-read-only-input"
+          label="Qualification"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div>
+      <TextField
+          id="outlined-read-only-input"
+          label="Guardian Name"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          id="outlined-read-only-input"
+          label="Batch-Number"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+         <TextField
+          id="outlined-read-only-input"
+          label="Guardian-PhoneNumber"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+       
+    
+      </div>
+    </Box>
+
     </div>
   )
 }
--- src/Admin/studentdetails.jsx
+++ src/Admin/studentdetails.jsx
@@ -14,7 +14,6 @@ import { setError, setLoading, setStudents } from '../Redux/Slices/studentSlice'
 import { useDispatch } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
 import { useRef } from 'react';
 import { Search } from '@mui/icons-material';
 import { useState } from 'react';
@@ -24,97 +23,104 @@ import { useState } from 'react';
 
 
 const Studentdetails = () => {
- 
-  const student = useSelector((state)=>state.student.students);
-    console.log("student details",student);
+
+  const student = useSelector((state) => state.student.students);
+  // console.log("student details",student);
   const loading = useSelector((state) => state.student.loading);
   const error = useSelector((state) => state.student.error);
 
+  //-----------------------------hooks------------------------------
+
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const inputRef = useRef();
-  
+  const [serachedData, setSearchedData] = useState([])
 
-  const handleSubmit = ()=>{
-  const search = inputRef.current.value.trim().toLowerCase()
-  const serachedData = student.filter((item)=>item.eMail.toLowerCase().includes(search))
-  console.log("searchedoutput",serachedData);
-}
+  //----------------------------------------------------------------
 
+  //----------------------handleSubmit for search--------------------
 
- 
-  useEffect(()=>{
-    const fetchData = async () =>{
+  const handleSubmit = () => {
+    const search = inputRef.current.value.trim().toLowerCase()
+    const serachedData = student.filter((item) => item.eMail.toLowerCase().includes(search))
+    setSearchedData(serachedData)
+  }
+  //-----------------------------------------------------------------
 
-      try{
+  //---------------fetching data from the server--------------------
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
         const response = await axios.get('http://localhost:3000/admin/student')
         dispatch(setStudents(response.data.data));
         dispatch(setLoading(false));
-  
-      }  catch(error){
+
+      } catch (error) {
         dispatch(setError(error));
         dispatch(setLoading(false))
       }
     };
     fetchData()
-  
-  },[dispatch])
 
-  
+  }, [dispatch])
+
+  //------------------------------------------------------------------
+
   return (
-    <div   style={{width:"80%"}} >
-    {loading ? (
-        <h1>Loading...</h1>
+    <div style={{ width: "80%" }} >
+      {loading ? (
+        <h3>Loading...</h3>
       ) : error ? (
         <p>Error: {error.message}</p>
       ) : (
-        
-        <TableContainer component={Paper}>
-        <Form className="d-flex">
-        <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                ref={inputRef}
-                onChange={handleSubmit}
-              />
-              {/* <Button variant="outline-success" >Search</Button> */}
-              </Form>
 
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-    
-        <TableHead>
-          <TableRow>
+        <TableContainer component={Paper}>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              ref={inputRef}
+              onChange={handleSubmit}
+            />
           
-            <TableCell>Index</TableCell>
-            <TableCell>User ID</TableCell>
-            <TableCell >User Name</TableCell>
-            <TableCell >Batch_Number</TableCell>
-            <TableCell >E-mail</TableCell>
-          
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {student.map((data,e) => (
-            <TableRow
-              key={data._id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-               {e+1}
-              </TableCell>
-              <TableCell onClick={()=>navigate('/singlestudentdetails')}> {data._id}</TableCell>
-              <TableCell >{data.userName}</TableCell>
-              <TableCell >{data.batch_Number}</TableCell>
-              <TableCell >{data.eMail}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </Form>
+
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+
+            <TableHead>
+              <TableRow>
+
+                <TableCell>Index</TableCell>
+                <TableCell>User ID</TableCell>
+                <TableCell >User Name</TableCell>
+                <TableCell >Batch_Number</TableCell>
+                <TableCell >E-mail</TableCell>
+
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(serachedData.length > 0 ? serachedData : student).map((data, e) => (
+                <TableRow
+                  key={data._id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {e + 1}
+                  </TableCell>
+                  <TableCell> {data._id}</TableCell>
+                  <TableCell >{data.userName}</TableCell>
+                  <TableCell >{data.batch_Number}</TableCell>
+                  <TableCell >{data.eMail}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
-      
+
     </div>
   )
 }
